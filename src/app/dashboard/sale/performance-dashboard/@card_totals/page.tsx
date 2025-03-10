6bb46352abc80ff81f745e406c6ca85196a79c27
MinJu <exzoqynnn.sarawut@hotmail.com>
2025-03-10 09:39:52 +0700

    ADD : Tooltip on care

--- src/app/dashboard/sale/performance-dashboard/@card_totals/page.tsx
+++ src/app/dashboard/sale/performance-dashboard/@card_totals/page.tsx
@@ -3,12 +3,34 @@ import { useEffect, useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 import { useSalePerformanceStore } from '@/app/contexts/useSalePerformanceStore';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip';
+import { Icons } from '@/components/icons';
 const titles = [
-  'Total Lead Sale',
-  'Total Survey',
-  'Total Quotation',
-  'Total Nurturing',
-  'Total Win'
+  {
+    name: 'Total Lead Sale',
+    description: 'จำนวนลูกค้าที่ชำระค่าบริการสำรวจพื้นที่'
+  },
+  {
+    name: 'Total Survey',
+    description: 'จำนวนลูกค้าที่ผ่านการสำรวจพื้นที่แล้ว'
+  },
+  {
+    name: 'Total Quotation',
+    description: 'จำนวนลูกค้าทั้งหมดที่ได้รับใบเสนอราคา'
+  },
+  {
+    name: 'Total Nurturing',
+    description: 'จำนวนลูกค้าที่ชำระมัดจำ 30%'
+  },
+  {
+    name: 'Total Win',
+    description: 'จำนวนลูกค้าที่อยู่ระหว่างการพิจารณาใบเสนอราคา'
+  }
 ];
 
 export default function CardTotals() {
@@ -35,8 +57,23 @@ export default function CardTotals() {
     <div className='grid gap-4 md:grid-cols-5 lg:grid-cols-5'>
       {titles.map((title, index) => (
         <Card key={index}>
-          <CardHeader className='flex justify-between pb-2'>
-            <CardTitle className='text-sm font-bold'>{title}</CardTitle>
+          <CardHeader className='grid justify-between pb-2'>
+            <CardTitle className='flex items-center text-sm font-bold'>
+              {title.name}
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <div className='ms-1'>
+                      {' '}
+                      <Icons.help className='h-[15px] w-[15px]' />
+                    </div>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p> {title.description}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </CardTitle>
           </CardHeader>
           <CardContent>
             <div className='text-2xl font-bold'>
